14cfc40dee2931598367be421063b3491b9e75f3
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2021-11-01 13:10:10 +0200

    Initial version with SvelteKit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import '../normalize.css';
+    import '../app.css';
+</script>
+
+<main>
+    <slot />
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<h1>Kohtuutalous</h1>
+<p>Kohtuutalous (eng. degrowth) tähtää ekologisesti kestävään, hyvinvoivaan ja demokraattiseen yhteiskuntaan, jossa talouden järjestelmät eivät perustu jatkuvan kasvun tavoitteluun. Kohtuuden keskiössä on sekä talousjärjestelmän että kulttuurin muutos. Ekologisesti kestävässä yhteiskunnassa nostetaan arvoon asioita, joita nykyisessä järjestelmässä ei huomioida riittävästi, kuten hoiva, ympäristönsuojelu, yhteisöllisyys ja vapaa-aika.</p>
+<p>Sivusto on rakenteilla, löydät lisää tietoa:</p>
+<ul>
+    <li><a href="https://kohtuusliike.fi">kohtuusliike.fi</a></li>
+    <li><a href="https://www.donitsitalous.fi">www.donitsitalous.fi</a></li>
+    <li><a href="https://degrowth.info">degrowth.info (englanniksi)</a></li>
+</ul>
